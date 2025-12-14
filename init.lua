bdcc49bfad51cd89197dbc2053f1e8ef4c15a9c3
Dynee <lucaspadden@gmail.com>
2025-12-13 22:08:27 -0500

    checkpoint: add keybind for navigating back

--- init.lua
+++ init.lua
@@ -559,6 +559,10 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          vim.keymap.set('n', 'gb', '<C-t>', {
+            desc = 'Go back',
+          })
+
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
           map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
