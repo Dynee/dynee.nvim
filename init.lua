9b220129b2d9900b3f2826ec1357906111b09d3e
Dynee <lucaspadden@gmail.com>
2026-01-11 17:22:13 -0500

    checkpoint: use one dark theme

--- init.lua
+++ init.lua
@@ -902,12 +902,13 @@ require('lazy').setup({
   },
 
   {
-    'morhetz/gruvbox',
-    name = 'gruvbox',
-    lazy = false,
-    priority = 1000,
+    'navarasu/onedark.nvim',
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.cmd.colorscheme 'gruvbox'
+      require('onedark').setup {
+        style = 'darker',
+      }
+      require('onedark').load()
     end,
   },
 
